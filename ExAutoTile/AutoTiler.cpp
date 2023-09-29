25a08bc3feadc4bc6d7b9bfd67a86d3d50df9e62
sashi0034 <satoshi3104.nishi@gmail.com>
2023-09-29 21:46:01 +0900

    バグ修正と描画部分実装

--- ExAutoTile/AutoTiler.cpp
+++ ExAutoTile/AutoTiler.cpp
@@ -11,31 +11,31 @@ namespace ExAutoTile
 		AngB = 1 << 3,
 	};
 
-	constexpr Point diagonalStart{4, 0};
+	constexpr Point diagonalStart{0, 4};
 
-	consteval Array<Point> getAutoTileFormat()
+	consteval std::array<Point, 16> getAutoTileFormat()
 	{
-		Array<Point> format(16);
+		std::array<Point, 16> format{};
 		format[0] = {0, 3};
 		format[AngB] = {0, 0};
 		format[AngB | AngT] = {0, 1};
 		format[AngT] = {0, 2};
-		format[AngR] = {1, 0};
-		format[AngR | AngL] = {2, 0};
-		format[AngL] = {3, 0};
-		format[AngL | AngB] = {1, 1};
-		format[AngR | AngB] = {3, 1};
-		format[AngL | AngT] = {1, 3};
-		format[AngR | AngT] = {3, 3};
-		format[AngL | AngT | AngR] = {2, 1};
-		format[AngT | AngR | AngB] = {1, 2};
-		format[AngT | AngL | AngB] = {3, 2};
-		format[AngL | AngB | AngR] = {2, 3};
-		format[AngL | AngR | AngT | AngB] = {2, 2};
+		format[AngR] = {1, 3};
+		format[AngR | AngL] = {2, 3};
+		format[AngL] = {3, 3};
+		format[AngR | AngB] = {1, 0};
+		format[AngL | AngB] = {3, 0};
+		format[AngR | AngT] = {1, 2};
+		format[AngL | AngT] = {3, 2};
+		format[AngL | AngB | AngR] = {2, 0};
+		format[AngT | AngR | AngB] = {1, 1};
+		format[AngT | AngL | AngB] = {3, 1};
+		format[AngL | AngT | AngR] = {2, 2};
+		format[AngL | AngR | AngT | AngB] = {2, 1};
 		return format;
 	}
 
-	static constexpr auto autoTileFormat = getAutoTileFormat();
+	static constexpr std::array<Point, 16> autoTileFormat = getAutoTileFormat();
 
 	Point GetAutoTileSrcPoint(int tileSize, const AutoTileConnect& connection)
 	{
@@ -50,16 +50,17 @@ namespace ExAutoTile
 
 	Point GetAutoTileDiagonalSrc(int tileSize, AutoTileDiagonal diagonal)
 	{
+		const auto start = tileSize * diagonalStart;
 		switch (diagonal)
 		{
 		case AutoTileDiagonal::LT:
-			return diagonalStart;
+			return start;
 		case AutoTileDiagonal::RT:
-			return diagonalStart.movedBy({tileSize / 2, 0});
+			return start.movedBy({tileSize / 2, 0});
 		case AutoTileDiagonal::LB:
-			return diagonalStart.movedBy({0, tileSize / 2});
+			return start.movedBy({0, tileSize / 2});
 		case AutoTileDiagonal::RB:
-			return diagonalStart.movedBy({tileSize / 2, tileSize / 2});
+			return start.movedBy({tileSize / 2, tileSize / 2});
 		default: ;
 			return {};
 		}
