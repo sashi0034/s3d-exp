b68cd3f0b3e538635a5a4e43fcc4cd7fb14569c5
sashi0034 <satoshi3104.nishi@gmail.com>
2023-09-29 22:34:25 +0900

    隅付きチップを対応

--- ExAutoTile/Main.cpp
+++ ExAutoTile/Main.cpp
@@ -4,6 +4,71 @@
 
 using namespace ExAutoTile;
 
+void drawTileAt(const Array<std::string>& tileMap, int y, int x, const Texture& texture)
+{
+	constexpr uint32 tileSize{24};
+	constexpr uint32 tileHalf{tileSize / 2};
+	(void)Rect(Point{x * tileSize, y * tileSize}, {tileSize, tileSize})
+		.draw(Palette::Antiquewhite);
+
+	constexpr char targetChar{'1'};
+	if (tileMap[y][x] == targetChar)
+	{
+		const AutoTileConnect connect{
+			.connectL = x == 0 || tileMap[y][x - 1] == targetChar,
+			.connectT = y == 0 || tileMap[y - 1][x] == targetChar,
+			.connectR = x == tileMap[y].size() - 1 || tileMap[y][x + 1] == targetChar,
+			.connectB = y == tileMap.size() - 1 || tileMap[y + 1][x] == targetChar,
+		};
+		const auto src = GetAutoTileSrcPoint(tileSize, connect);
+
+		const bool isSafeRange = 0 < x && x < tileMap[y].size() - 1 && 0 < y && y < tileMap.size() - 1;
+		const bool diagonalLT = isSafeRange && connect.connectL && connect.connectT &&
+			tileMap[y - 1][x - 1] != targetChar;
+		const bool diagonalRT = isSafeRange && connect.connectR && connect.connectT &&
+			tileMap[y - 1][x + 1] != targetChar;
+		const bool diagonalLB = isSafeRange && connect.connectL && connect.connectB &&
+			tileMap[y + 1][x - 1] != targetChar;
+		const bool diagonalRB = isSafeRange && connect.connectR && connect.connectB &&
+			tileMap[y + 1][x + 1] != targetChar;
+
+		if (diagonalLT || diagonalRT || diagonalLB || diagonalRB)
+		{
+			// 隅付き描画
+			(void)texture(
+					diagonalLT
+						? GetAutoTileDiagonalSrc(tileSize, AutoTileDiagonal::LT)
+						: src.movedBy(0, 0),
+					Size{tileHalf, tileHalf})
+				.draw(Point{x * tileSize, y * tileSize}.movedBy(0, 0));
+			(void)texture(
+					diagonalRT
+						? GetAutoTileDiagonalSrc(tileSize, AutoTileDiagonal::RT)
+						: src.movedBy(tileHalf, 0),
+					Size{tileHalf, tileHalf})
+				.draw(Point{x * tileSize, y * tileSize}.movedBy(tileHalf, 0));
+			(void)texture(
+					diagonalLB
+						? GetAutoTileDiagonalSrc(tileSize, AutoTileDiagonal::LB)
+						: src.movedBy(0, tileHalf),
+					Size{tileHalf, tileHalf})
+				.draw(Point{x * tileSize, y * tileSize}.movedBy(0, tileHalf));
+			(void)texture(
+					diagonalRB
+						? GetAutoTileDiagonalSrc(tileSize, AutoTileDiagonal::RB)
+						: src.movedBy(tileHalf, tileHalf),
+					Size{tileHalf, tileHalf})
+				.draw(Point{x * tileSize, y * tileSize}.movedBy(tileHalf, tileHalf));
+		}
+		else
+		{
+			// 通常描画
+			(void)texture(src, Size{tileSize, tileSize})
+				.draw(Point{x * tileSize, y * tileSize});
+		}
+	}
+}
+
 void Main()
 {
 	Scene::SetBackground(ColorF{0.3, 0.3, 0.3});
@@ -27,28 +92,12 @@ void Main()
 	{
 		camera.update();
 		const auto t = camera.createTransformer();
+		SamplerState sample{SamplerState::BorderNearest};
 		for (int y = 0; y < tileMap.size(); ++y)
 		{
-			const auto& checking = tileMap[y];
-			for (int x = 0; x < checking.size(); ++x)
+			for (int x = 0; x < tileMap[y].size(); ++x)
 			{
-				constexpr uint32 tileSize{24};
-				(void)Rect(Point{x * tileSize, y * tileSize}, {tileSize, tileSize})
-					.draw(Palette::Antiquewhite);
-
-				constexpr char targetChar{'1'};
-				if (checking[x] == targetChar)
-				{
-					AutoTileConnect connect{
-						.connectL = x == 0 || tileMap[y][x - 1] == targetChar,
-						.connectT = y == 0 || tileMap[y - 1][x] == targetChar,
-						.connectR = x == checking.size() - 1 || tileMap[y][x + 1] == targetChar,
-						.connectB = y == tileMap.size() - 1 || tileMap[y + 1][x] == targetChar,
-					};
-					const auto src = GetAutoTileSrcPoint(tileSize, connect);
-					(void)texture(src, Size{tileSize, tileSize})
-						.draw(Point{x * tileSize, y * tileSize});
-				}
+				drawTileAt(tileMap, y, x, texture);
 			}
 		}
 	}
