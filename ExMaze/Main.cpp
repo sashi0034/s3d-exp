31fb8c595a4cf92f1a667a6f580e8ba50e6f6356
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-03 16:46:23 +0900

    迷路生成のプロジェクト追加

---
+++ ExMaze/Main.cpp
@@ -0,0 +1,54 @@
+﻿#include "stdafx.h" // OpenSiv3D v0.6.10
+
+#include "MazeGenerator.h"
+
+using namespace ExMaze;
+
+void Main()
+{
+	Scene::SetBackground(ColorF{0.3, 0.3, 0.3});
+
+	const Texture emojiWall{U"🧱"_emoji};
+	const Texture emojiFloor{U"🟦"_emoji};
+	const Texture emojiPathway{U"🟩"_emoji};
+
+	auto mazeGenProps = MazeGenProps{
+		.size = {81, 81},
+	};
+
+	MapGrid grid{{}};
+	Camera2D camera{Scene::Center(), 1.0};
+
+	while (System::Update())
+	{
+		camera.update();
+		{
+			const auto t = camera.createTransformer();
+
+			for (int x = 0; x < grid.Data().size().x; ++x)
+			{
+				for (int y = 0; y < grid.Data().size().y; ++y)
+				{
+					auto&& cell = grid.At({x, y});
+					constexpr int size = 12;
+					switch (cell.kind)
+					{
+					case TerrainKind::Wall:
+						(void)emojiWall.resized(size, size).draw(x * size, y * size);
+						break;
+					case TerrainKind::Floor:
+						(void)emojiFloor.resized(size, size).draw(x * size, y * size);
+						break;
+					default: ;
+					}
+				}
+			}
+		}
+
+		if (SimpleGUI::Button(U"生成", {20, 20}) ||
+			grid.HasData() == false)
+		{
+			grid = GenerateFreshMaze(mazeGenProps);
+		}
+	}
+}
---
+++ ExMaze/MazeGenerator.cpp
@@ -0,0 +1,85 @@
+﻿#include "stdafx.h"
+#include "MazeGenerator.h"
+
+namespace ExMaze
+{
+	class MazeGenInternal;
+
+	struct MazeGenState
+	{
+		Grid<bool> dugFlag;
+	};
+}
+
+class ExMaze::MazeGenInternal
+{
+public:
+	explicit MazeGenInternal(const MazeGenProps& props): props(props)
+	{
+	}
+
+	void createRooms(MazeGenState& state) const
+	{
+		for (auto i : step(props.numRooms))
+		{
+			int x, y;
+			while (true)
+			{
+				constexpr int padding = 3;
+				x = padding + (Random(0, props.size.x - padding) / 2) * 2;
+				y = padding + (Random(0, props.size.y - padding) / 2) * 2;
+				if (canDigRoom(state, {{x - 2, y - 2}, {props.roomSize + 4, props.roomSize + 4}}))
+					break;
+			}
+
+			for (auto p : step({x, y}, {props.roomSize, props.roomSize}))
+			{
+				digAt(state, p);
+			}
+		}
+	}
+
+private:
+	const MazeGenProps& props;
+
+	bool canDigRoom(const MazeGenState& state, const Rect& rect) const
+	{
+		for (auto p : step(rect.pos, rect.size / 2, {2, 2}))
+		{
+			if (canDigAt(state, p) == false) return false;
+		}
+		return true;
+	}
+
+	bool canDigAt(const MazeGenState& state, const Point& p) const
+	{
+		if (p.x < 0 || props.size.x <= p.x ||
+			p.y < 0 || props.size.y <= p.y)
+			return false;
+		return state.dugFlag[p] == false;
+	}
+
+	static void digAt(MazeGenState& state, const Point& p)
+	{
+		state.dugFlag[p] = true;
+	}
+};
+
+namespace ExMaze
+{
+	MapGrid GenerateFreshMaze(const MazeGenProps& props)
+	{
+		const MazeGenInternal internal{props};
+		MazeGenState state{
+			.dugFlag = Grid<bool>{props.size}
+		};
+		internal.createRooms(state);
+
+		MapGrid grid{props.size};
+		for (auto p : step({0, 0}, props.size))
+		{
+			if (state.dugFlag[p]) grid.At(p).kind = TerrainKind::Floor;
+		}
+		return grid;
+	}
+}
