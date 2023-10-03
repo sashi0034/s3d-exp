832349d0e9fd601dfb5e36e41433f2a77101fcdb
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-03 21:37:05 +0900

    迷路がちゃんと生成されるように実装

--- ExMaze/MazeGenerator.cpp
+++ ExMaze/MazeGenerator.cpp
@@ -8,7 +8,19 @@ namespace ExMaze
 	struct MazeGenState
 	{
 		Grid<bool> dugFlag;
+		Grid<bool> roomEntryFlag;
 	};
+
+	Point IndexToDirection(int index)
+	{
+		static constexpr std::array a{
+			Point{1, 0},
+			Point{0, 1},
+			Point{-1, 0},
+			Point{0, -1},
+		};
+		return a[index];
+	}
 }
 
 class ExMaze::MazeGenInternal
@@ -36,6 +48,40 @@ public:
 			{
 				digAt(state, p);
 			}
+
+			if (Random(0, 1) == 0)
+			{
+				state.roomEntryFlag[{
+					x + Random(0, props.roomSize / 2) * 2, y + (props.roomSize / 2) * 2 * Random(0, 1)
+				}] = true;
+			}
+			else
+			{
+				state.roomEntryFlag[{
+					x + (props.roomSize / 2) * 2 * Random(0, 1), y + Random(0, props.roomSize / 2) * 2
+				}] = true;
+			}
+		}
+	}
+
+	void digPassageCompletely(MazeGenState& state) const
+	{
+		Point p1;
+		while (true)
+		{
+			p1.x = 1 + Random(0, (props.size.x) / 2 - 1) * 2;
+			p1.y = 1 + Random(0, (props.size.y) / 2 - 1) * 2;
+			if (canDigAt(state, p1) == false)
+			{
+				// 点P1から掘れるだけ掘る
+				while (true)
+				{
+					const auto p2 = digStroke(state, p1);
+					if (p1 == p2) break;
+					p1 = p2;
+				}
+			}
+			if (hasDugCompletely(state)) break;
 		}
 	}
 
@@ -51,17 +97,69 @@ private:
 		return true;
 	}
 
+	bool isRangedPoint(const Point& p) const
+	{
+		return 0 <= p.x && p.x < props.size.x &&
+			0 <= p.y && p.y < props.size.y;
+	}
+
 	bool canDigAt(const MazeGenState& state, const Point& p) const
 	{
-		if (p.x < 0 || props.size.x <= p.x ||
-			p.y < 0 || props.size.y <= p.y)
-			return false;
+		if (isRangedPoint(p) == false) return false;
 		return state.dugFlag[p] == false;
 	}
 
 	static void digAt(MazeGenState& state, const Point& p)
 	{
 		state.dugFlag[p] = true;
+	}
+
+	Point digStroke(MazeGenState& state, Point p1) const
+	{
+		const auto dig = [](MazeGenState& state, const Point& p1, const Point& d)
+		{
+			digAt(state, p1 + d * 1);
+			digAt(state, p1 + d * 2);
+			return p1 + d * 2;
+		};
+		Point p2 = p1;
+		Array<int> dirs{0, 1, 2, 3};
+		dirs.shuffle();
+
+		for (int i = 0; i < dirs.size(); ++i)
+		{
+			const auto d = IndexToDirection(dirs[i]);
+			if (isRangedPoint(p1 + d * 2) &&
+				state.roomEntryFlag[p1 + d * 2] && state.dugFlag[p1 + d * 1] == false)
+			{
+				// 部屋作成時のフラグが存在していたら優先的に掘る
+				return dig(state, p1, d);
+			}
+		}
+
+		for (int i = 0; i < dirs.size(); ++i)
+		{
+			const auto d = IndexToDirection(dirs[i]);
+			if (canDigAt(state, p1 + d * 2))
+			{
+				// 掘れる方向なら掘る
+				return dig(state, p1, d);
+			}
+		}
+
+		return p1;
+	}
+
+	bool hasDugCompletely(const MazeGenState& state) const
+	{
+		for (int x = 1; x < props.size.x; x += 2)
+		{
+			for (int y = 1; y < props.size.y; y += 2)
+			{
+				if (canDigAt(state, {x, y})) return false;
+			}
+		}
+		return true;
 	}
 };
 
@@ -71,9 +169,11 @@ namespace ExMaze
 	{
 		const MazeGenInternal internal{props};
 		MazeGenState state{
-			.dugFlag = Grid<bool>{props.size}
+			.dugFlag = Grid<bool>{props.size},
+			.roomEntryFlag = Grid<bool>{props.size},
 		};
 		internal.createRooms(state);
+		internal.digPassageCompletely(state);
 
 		MapGrid grid{props.size};
 		for (auto p : step({0, 0}, props.size))
