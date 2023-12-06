72d58f5f0e1204bcbe60777c423f8707a751d1c1
sashi0034 <satoshi3104.nishi@gmail.com>
2023-12-06 10:38:59 +0900

    基本的な部分

---
+++ ExIllustMotion/App/asset/illust_motion.hlsl
@@ -0,0 +1,124 @@
+﻿Texture2D g_texture0 : register(t0);
+SamplerState g_sampler0 : register(s0);
+
+namespace s3d
+{
+    //
+    //  VS Input (nothing)
+    //
+
+    //
+    //  VS Output
+    //
+    struct PSInput
+    {
+        float4 position : SV_POSITION;
+        float4 color : COLOR0;
+        float2 uv : TEXCOORD0;
+    };
+
+    //
+    //  Siv3D Functions
+    //
+    float4 Transform2D(float2 pos, float2x4 t)
+    {
+        return float4((t._13_14 + (pos.x * t._11_12) + (pos.y * t._21_22)), t._23_24);
+    }
+}
+
+//
+//  Constant Buffer
+//
+cbuffer VSConstants2D : register(b0)
+{
+    row_major float2x4 g_transform;
+    float4 g_colorMul;
+}
+
+cbuffer IllustMotionVsCb : register(b1)
+{
+    int g_divideCount;
+}
+
+cbuffer PSConstants2D : register(b0)
+{
+    float4 g_colorAdd;
+    float4 g_sdfParam;
+    float4 g_sdfOutlineColor;
+    float4 g_sdfShadowColor;
+    float4 g_internal;
+}
+
+float2 toUV(int quadId, uint mod6)
+{
+    const float step = 1.0 / g_divideCount;
+    const float2 offset = float2(step, step) * float2(quadId % g_divideCount, quadId / g_divideCount);
+
+    if (mod6 == 0)
+    {
+        return offset + float2(0, 0);
+    }
+    else if (mod6 == 1)
+    {
+        return offset + float2(step, 0);
+    }
+    else if (mod6 == 2)
+    {
+        return offset + float2(0, step);
+    }
+    else if (mod6 == 3)
+    {
+        return offset + float2(0, step);
+    }
+    else if (mod6 == 4)
+    {
+        return offset + float2(step, 0);
+    }
+    else
+    {
+        return offset + float2(step, step);
+    }
+}
+
+float2 toQuad(float2 uv, float2 size)
+{
+    float2 halfSize = size / 2.0;
+    float2 ret = lerp(-halfSize, halfSize, uv);
+    return ret;
+}
+
+// 頂点シェーダー
+s3d::PSInput VS(uint id: SV_VERTEXID)
+{
+    s3d::PSInput result;
+
+    // 四角形一つの頂点は6 
+    const uint quadId = id / 6;
+    const uint mod6 = id % 6;
+
+    // 座標を適当に計算
+    float2 pos = float2(0, 0);
+    const int xId = (int)(quadId % g_divideCount);
+    const int yId = (int)(quadId / g_divideCount);
+
+    float2 size = float2(40, 40);
+    pos += float2(xId * size.x, yId * size.y);
+
+    // uv座標を計算
+    const float2 uv = toUV(quadId, mod6);
+
+    pos += toQuad(uv, size);
+
+    result.position = s3d::Transform2D(pos, g_transform);
+    result.uv = uv;
+    result.color = g_colorMul;
+
+    return result;
+}
+
+// ピクセルシェーダー
+float4 PS(s3d::PSInput input) : SV_TARGET
+{
+    const float4 texColor = g_texture0.Sample(g_sampler0, input.uv);
+    return (texColor * input.color) + g_colorAdd;
+}
