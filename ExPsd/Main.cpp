60fdc5f1726f5b85b51b71b3b00f3702df8c4885
sashi0034 <satoshi3104.nishi@gmail.com>
2023-12-19 14:31:39 +0900

    PSDプロジェクト追加

---
+++ ExPsd/Main.cpp
@@ -0,0 +1,14 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.12
+#include "PsdSample.h"
+
+void Main()
+{
+	Scene::SetBackground(ColorF{0.3});
+
+	int result = StartSample();
+	Print(result);
+
+	while (System::Update())
+	{
+	}
+}
